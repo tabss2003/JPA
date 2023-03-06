2e8d174f28458776124cfeee3dd7dc75de39bf2f
tabss2003 <tabss2003@naver.com>
2023-03-06 23:39:44 +0900

    Update : 엔티티 설계시 주의점

--- jpabook/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpabook/src/main/java/jpabook/jpashop/domain/Order.java
@@ -16,13 +16,13 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order",cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -31,4 +31,19 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status; //주문 상태 ORDER, CANCEL
 
+    //==연관관계 메서드 : 컨트롤 하는 쪽이 가지고 있는 게 좋다.==//
+    public void setMember(Member member){
+        this.member =member;
+        member.getOrdes().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }

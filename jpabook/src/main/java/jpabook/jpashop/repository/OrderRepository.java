02263d4adce4a3b107e514906c8d472ae4a9a52e
tabss2003 <tabss2003@naver.com>
2023-03-19 22:20:09 +0900

    Update : 주문조회 V2: entity -> DTO

--- jpabook/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpabook/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -97,7 +97,7 @@ public class OrderRepository {
         ).getResultList();
     }
 
-    
+
     public List<OrderSimpleQueryDto> findOrderDtos() {
         return em.createQuery(
                 "select new jpabook.jpashop.repository.OrderSimpleQueryDto(o.id, m.name,o.orderDate,o.status, d.address) from Order o"+

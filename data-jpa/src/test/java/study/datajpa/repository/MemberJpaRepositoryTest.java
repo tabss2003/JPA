9d35e414e9f3c2083fa8137d3f87650130f0530c
tabss2003 <tabss2003@naver.com>
2023-03-22 12:12:02 +0900

    Update : 순수 JPA기반 리포지토리 만들기

--- data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -8,6 +8,8 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 
 @SpringBootTest
 @Transactional
@@ -23,5 +25,34 @@ class MemberJpaRepositoryTest {
         Assertions.assertEquals(findMember.getId(),member.getId());
         Assertions.assertEquals(findMember.getUsername(),member.getUsername());
         Assertions.assertEquals(findMember,member);
+    }
+    @Test
+    public void basicCRUD(){
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        // 단건 조회
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+        Assertions.assertEquals(findMember1,member1);
+        Assertions.assertEquals(findMember2,member2);
+
+        //리스트 조회
+        List<Member> all = memberJpaRepository.findAll();
+        Assertions.assertEquals(all.size(),2);
+
+        //카운트
+        long count = memberJpaRepository.count();
+        Assertions.assertEquals(count,2);
+
+        //삭제
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+
+        long deleteCount = memberJpaRepository.count();
+        Assertions.assertEquals(deleteCount,0);
+
     }
 }
